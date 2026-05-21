df139c82d344d78a1c7d6d3690b849b093836e8d
Minhaj Ap <minhajap00@gmail.com>
2026-05-21 15:24:47 +0530

    Moved Token refresh logic to auth file

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,3 +37,48 @@ export async function SignIn(username: string, password: string) {
 
   return data;
 }
+
+async function RefreshToken() {
+  const response = await fetch(`${SERVER_URL}api/token/refresh/`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    credentials: "include",
+  });
+  if (!response.ok) {
+    throw new Error(`Token refresh failed: ${response.statusText}`);
+  }
+  const data = await response.json();
+
+  return data;
+}
+
+export async function fetchWithAuth(
+  url: string,
+  options: RequestInit = {},
+  body: unknown = null,
+) {
+  const response = await fetch(url, {
+    ...options,
+    headers: {
+      ...options.headers,
+    },
+    body: body ? JSON.stringify(body) : options.body,
+    credentials: "include",
+  });
+  if (response.status === 401) {
+    try {
+      await RefreshToken();
+      return await fetchWithAuth(url, options);
+    } catch (error) {
+      //   window.location.href = "/";
+      console.error("Token refresh failed:", error);
+      throw new Error("Authentication failed. Please log in again.");
+    }
+  }
+  if (!response.ok) {
+    throw new Error(`Request failed: ${response.statusText}`);
+  }
+  return response;
+}
--- src/lib/feedback.ts
+++ src/lib/feedback.ts
@@ -1,5 +1,6 @@
 import { SERVER_URL } from "@/constants";
 import { CreateFeedbackPayload, Feedback } from "@/types";
+import { fetchWithAuth } from "./auth";
 
 export async function GetFeedback(): Promise<Feedback[]> {
   const response = await fetchWithAuth(`${SERVER_URL}api/feedbacks/`, {
@@ -38,47 +39,3 @@ export async function CreateFeedback(
   return data;
 }
 
-async function RefreshToken() {
-  const response = await fetch(`${SERVER_URL}api/token/refresh/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    credentials: "include",
-  });
-  if (!response.ok) {
-    throw new Error(`Token refresh failed: ${response.statusText}`);
-  }
-  const data = await response.json();
-
-  return data;
-}
-
-export async function fetchWithAuth(
-  url: string,
-  options: RequestInit = {},
-  body: any = null,
-) {
-  const response = await fetch(url, {
-    ...options,
-    headers: {
-      ...options.headers,
-    },
-    body: body ? JSON.stringify(body) : options.body,
-    credentials: "include",
-  });
-  if (response.status === 401) {
-    try {
-      await RefreshToken();
-      return await fetchWithAuth(url, options);
-    } catch (error) {
-      //   window.location.href = "/";
-      console.error("Token refresh failed:", error);
-      throw new Error("Authentication failed. Please log in again.");
-    }
-  }
-  if (!response.ok) {
-    throw new Error(`Request failed: ${response.statusText}`);
-  }
-  return response;
-}
